9561e1cbb7db03e7ed4a5c222170b0291b0de8d1
Tristan B. Kildaire <deavmi@disroot.org>
2020-10-23 22:04:22 +0200

    Stub

---
+++ source/UserDirectory.d
@@ -0,0 +1,35 @@
+module UserDirectory;
+
+import Connection;
+import gtk.SearchBar;
+import gtk.Entry;
+import gtk.Window;
+
+public final class UserDirectory
+{
+    /* The associated connection */
+    private Connection connection;
+
+    this(Connection connection)
+    {
+        this.connection = connection;
+
+        initWindow();
+    }
+
+    private void initWindow()
+    {
+        Window userWindow = new Window(GtkWindowType.TOPLEVEL);
+        userWindow.setTitle("User directory");
+
+        SearchBar searchBar = new SearchBar();
+        Entry searchEntry = new Entry();
+        searchEntry.setText("fsdhjsdfhjkdsfhjksdfhjk");
+        // searchBar.handleEvent()
+        searchBar.connectEntry(searchEntry);
+        userWindow.add(searchBar);
+
+
+        userWindow.showAll();
+    }
+}
