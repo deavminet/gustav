d4912089b1021cb2018f2b156141d2887b123273
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-28 11:33:11 +0200

    WIP: Message areas

---
+++ source/MessageArea.d
@@ -0,0 +1,13 @@
+/**
+* MessageArea
+*
+* Represents the binding of a text entry, send button, user list (sometimes)
+* and message log - basically the place where you message a channel or someone
+*
+* The sub-classes are "Direct Message" and "Channel"
+*/
+
+public class MessageArea
+{
+    /* TODO: Implement me */
+}
