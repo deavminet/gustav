be543d33fd2589db87ec0012deec2e361320b833
Tristan B. Kildaire <deavmi@disroot.org>
2021-01-28 11:44:00 +0200

    Missing imports

--- source/areas/User.d
+++ source/areas/User.d
@@ -2,6 +2,27 @@ module areas.User;
 
 import areas.MessageArea;
 
+import gtk.Box;
+import gtk.ListBox;
+import gtk.Label;
+import gtk.TextView;
+import libdnet.dclient;
+import gtk.Label;
+import std.string;
+import gtk.Button;
+import gtk.Tooltip;
+import gtk.Widget;
+import gtk.ScrolledWindow;
+import gtk.Button;
+import gtk.Entry;
+import UserNode;
+
+import pango.PgAttributeList;
+import pango.PgAttribute;
+import Connection;
+
+import gogga;
+
 public final class User : MessageArea
 {
     private DClient client;
@@ -39,5 +60,6 @@ public final class User : MessageArea
 
     private void initializeBox()
     {
-        
+
     }
+}
